e4c60fecc244dacf9151bb0971c1ba4f7c9406d3
woogi-at-518915832653 <jwchoi72@lotte.net>
2019-04-11 08:44:53 +0900

    Test Environments Setting

--- rabbitmq_poc/src/test/java/com/breezelab/springboot/rabbitmq/RabbitmqPocApplicationTests.java
+++ rabbitmq_poc/src/test/java/com/breezelab/springboot/rabbitmq/RabbitmqPocApplicationTests.java
@@ -4,13 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class RabbitmqPocApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+        assertTrue(true);
+    }
 }
