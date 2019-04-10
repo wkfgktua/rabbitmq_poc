4b2e35c551f3c0a90c00943a11b9241a76f4234a
woogi-at-518915832653 <jwchoi72@lotte.net>
2019-04-11 08:39:12 +0900

    Scenario #1 Source update

--- rabbitmq_poc/src/main/java/com/breezelab/springboot/rabbitmq/RabbitmqPocApplication.java
+++ rabbitmq_poc/src/main/java/com/breezelab/springboot/rabbitmq/RabbitmqPocApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RabbitmqPocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RabbitmqPocApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqPocApplication.class, args);
+    }
 }
+
